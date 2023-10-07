cf8da3ebc1eb76cc22f4c33d86b7dfab0037c63f
khalid2000-t <khalid.m7maed@gmail.com>
2023-10-06 19:41:15 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: always return 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
