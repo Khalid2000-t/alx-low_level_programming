cb25a0759df5440370a92c529aa766bab813279a
khalid2000-t <khalid.m7maed@gmail.com>
2023-10-06 19:36:25 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+/**
+ * main - print all single digits base on 10 and zero
+ * Return: always return 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x + '0');
+	putchar('\n');
+
+	return (0);
+}
