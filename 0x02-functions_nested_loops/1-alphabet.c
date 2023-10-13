566649036960f2bbc8b4a6032e49c90eee3c2555
khalid2000-t <khalid.m7maed@gmail.com>
2023-10-12 17:18:22 -0700

    0-isupper

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return:void 
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (ch = 'a'; c <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
