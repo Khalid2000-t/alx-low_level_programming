676f054b94c365cfa1147fe55975d08494efa58d
khalid2000-t <khalid.m7maed@gmail.com>
2023-10-18 14:07:01 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - function
+ * @s: string
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	while (s[i] != '\0')
+	{
+		if (i == 0 || strchr(sep, s[i - 1]))
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
