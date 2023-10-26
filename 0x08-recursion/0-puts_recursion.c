71ac58d408c57b972d9235d85cac286bdd101f68
khalid2000-t <khalid.m7maed@gmail.com>
2023-10-25 18:16:49 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+include "main.h"
+/**
+ * _puts_recursion - fumction
+ * @s: char
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		if (*s == '\0')
+		{	_putchar('\n');
+			return;
+		}
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
